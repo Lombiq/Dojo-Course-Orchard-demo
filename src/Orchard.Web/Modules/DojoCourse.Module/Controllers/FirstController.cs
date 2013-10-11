21cc8dec1c117e3c04fba3591c10f0f3e651d745
Benedek Farkas <bfarkas@windowslive.com>
2013-10-12 01:09:33 +0200

    Lesson 3: Adding updated theme and adding module

---
+++ src/Orchard.Web/Modules/DojoCourse.Module/Controllers/FirstController.cs
@@ -0,0 +1,31 @@
+﻿using Orchard;
+using Orchard.Themes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DojoCourse.Module.Controllers
+{
+    [Themed]
+    public class FirstController : Controller
+    {
+        private IWorkContextAccessor _wca;
+
+
+        public FirstController(IWorkContextAccessor wca)
+        {
+            _wca = wca;
+        }
+
+
+        public ViewResult Index()
+        {
+            var workContext = _wca.GetContext();
+            var siteName = workContext.CurrentSite.SiteName;
+
+            return View();
+        }
+    }
+}
